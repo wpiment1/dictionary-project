99c38a3b4faa8b97a55001c5ee2d5590f711a99c
wpiment1 <winifer.pimentel@gmail.com>
2022-02-02 16:08:42 -0500

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Dictionary />
+      <footer>Coded by Winifer Pimentel</footer>
+    </div>
+  );
 }
